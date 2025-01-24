efa38739b621ec74bc1a6efcf00b12dfeb2815da
Tuan Nguyen <tuannguyen08426@gmail.com>
2025-01-24 08:42:15 -0600

    Inital files

---
+++ R/mysq.R
@@ -0,0 +1,12 @@
+#' Mysquare function
+#'
+#' @param x A quantitative vector.
+#'
+#' @returns A quantitative vector with squared components.
+#' @export
+#'
+#' @examples
+#' mysq(1:10)
+mysq <- function(x){
+  x^2
+}
